e7ae9ca8425ecc2951af0a666204c27526c1b6b7
Simon <simon.h.rosner@gmail.com>
2017-12-22 03:17:57 -0500

    Bug fixes and demo added Still need to deal with character encoding

--- gooseLeader.py
+++ gooseLeader.py
@@ -11,55 +11,52 @@ except ImportError:
 #twitter stuff
 from twitter import Twitter, OAuth, TwitterHTTPError
 
-class Scope:
+#filename and pathing stuff
+import os
+import os.path
+
+class gooseLeader:
     """Allows for searches of tweets and specific manipulations search results"""
     def __init__(self, 
                  accessToken = None,
                  accessSecret = None,
                  consumerKey = None,
                  consumerSecret = None):
-        #get tokens, keys, secrets from init.txt file
+        #get tokens, keys, secrets from init.txt file where applicable
         file = open('init.txt', 'r')
-        if accessToken is None:
-            for line in file:
-                spot = line.find('=') + 1
-                if 'accessToken= ' in line:
-                    accessToken = line[spot:]
-        if accessSecret is None:
-            for line in file:
-                spot = line.find('=') + 1
-                if 'accessSecret= ' in line:
-                    accessSecret = line[spot:]
-        if consumerKey is None:
-            for line in file:
-                spot = line.find('=') + 1
-                if 'consumerKey= ' in line:
-                    consumerKey = line[spot:]
-        if consumerSecret is None:
-            for line in file:
-                spot = line.find('=') + 1
-                if 'consumerSecret= ' in line:
-                    consumerSecret = line[spot:]
+        for line in file:
+            spot = line.find('=') + 1
+            if 'accessToken=' in line and accessToken is None:
+                    accessToken = line[spot:].strip()
+            elif 'accessSecret=' in line and accessSecret is None:
+                    accessSecret = line[spot:].strip()
+            elif 'consumerKey=' in line and consumerKey is None:
+                    consumerKey = line[spot:].strip()
+            elif 'consumerSecret=' in line and consumerSecret is None:
+                    consumerSecret = line[spot:].strip()
+
+        print(",",accessToken,",",accessSecret,",",consumerKey,",",consumerSecret)
         oauth = OAuth(accessToken, accessSecret, consumerKey, consumerSecret)
 
         #twitter search
         self.twitter = Twitter(auth=oauth)
 
-    def __search(self, query, tweetCount, langauge = None, resultType = None):
+    def search(self, query, tweetCount, language = None, resultType = None):
         """searches using twitter REST API"""
         #optional argument check
-        if language == None: langauge = ''
+        if language == None: language = ''
         if resultType == None: resultType = ''
         
-        tweets = twitter.search.tweets(q = query, result_type = resultType,
+        tweets = self.twitter.search.tweets(q = query, result_type = resultType,
                                             lang=language, count=tweetCount)
+        tweets = tweets
         return tweets
 
     def getInfluUsers(self, userCount, tweets):
         """Returns info on most influential users in a given search"""
         influUsers = []
-        for user in tweets['user']:
-            influUsers.append(user)
+        for user in tweets['statuses']:
+            influUsers.append(user['user'])
             #sort
             influUsers = sorted(influUsers, key = lambda user: user['followers_count'], reverse = True)
             #get top userCount
@@ -68,21 +65,22 @@ class Scope:
 
     def userHTMLReport(self, users, title = None):
         """Creates an Html file with details on users"""
-        filename = (title, "report.html")
+        filename = (title + "report.html")
         with open(filename,'w+') as file:
             file.write("<!DOCTYPE html><html><head>")
-            if title != None: file.write("<title>",title,"</title>")
+            if title != None: file.write("<title>"+title+"</title>")
             file.write("</head><body>")
             for user in users:
-                file.write("<div><p><a href='https://twitter.com/",user['screen_name'],"'>")
-                file.write("<img src='",user['profile_image_url_https'],"'></a></p>")
-                file.write("<p>Name: ", user['name'],"</p>")
-                file.write("<p>Username: ", user['screen_name'],"</p>")
-                file.write("<p>Verified: ", user['verified'],"</p>")
-                file.write("<p>Contributors: ", user['contributors_enabled'],"</p>")
-                file.write("<p>Location: ", user['location'],"</p>")
-                file.write("<p>Followers: ", user['followers_count'],"</p>")          
-                file.write("<p>Description: ", user['description'],"</p>")
-                if user['url'] != null: file.write("<p>Url: ", user['url'],"</p>")          
+                file.write("<div><p><a href='https://twitter.com/"+user['screen_name']+"'>")
+                file.write("<img src='"+user['profile_image_url_https']+"'></a></p>")
+                file.write("<p>Name: "+ user['name']+"</p>")
+                file.write("<p>Username: "+ user['screen_name']+"</p>")
+                file.write("<p>Verified: "+ str(user['verified'])+"</p>")
+                file.write("<p>Contributors: "+ str(user['contributors_enabled'])+"</p>")
+                file.write("<p>Location: "+ user['location']+"</p>")
+                file.write("<p>Followers: "+ str(user['followers_count'])+"</p>")          
+                file.write("<p>Description: "+ user['description']+"</p>")
+                if user['url'] != 'null':
+                    file.write("<p>Url: <a href='"+ str(user['url'])+"'>"+str(user['url'])+"</a></p>")          
                 file.write("</div>")
             file.write("</body></html>")
